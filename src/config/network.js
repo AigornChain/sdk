fc7ceadd4bfcc835d2ee06ccf652bdd309d5a1c5
Aigorn <parereprime677@gmail.com>
2026-03-16 20:49:58 +0700

    Create network.js

---
+++ src/config/network.js
@@ -0,0 +1,10 @@
+export const networks = {
+    ethereum: {
+        chainId: 1,
+        rpc: "https://rpc.ankr.com/eth"
+    },
+    solana: {
+        chainId: "solana",
+        rpc: "https://api.mainnet-beta.solana.com"
+    }
+}
